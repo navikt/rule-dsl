a4b0521d23d26645e22d0cbac6a2edfb6b17b943
Jens.Schiefloe <jens.schiefloe@nav.no>
2025-10-27 18:35:02 +0100

    Lagt til manglende faktum operatorer.

--- src/main/kotlin/no/nav/system/rule/dsl/rettsregel/Operators.kt
+++ src/main/kotlin/no/nav/system/rule/dsl/rettsregel/Operators.kt
@@ -4,23 +4,89 @@ import no.nav.system.rule.dsl.enums.ListComparator.ER_BLANDT
 import no.nav.system.rule.dsl.enums.ListComparator.ER_IKKE_BLANDT
 import no.nav.system.rule.dsl.enums.PairComparator.*
 import java.time.LocalDate
+import java.time.YearMonth
 
 /**
- * Datoer
+ * Datoer (localDate)
  */
+@JvmName("faktum_localdate_erFørEllerLik_faktum_localdate")
 infix fun Faktum<LocalDate>.erFørEllerLik(other: Faktum<LocalDate>) =
     PairDomainPredicate(FØR_ELLER_LIK, this, other) { this.value <= other.value }
 
+@JvmName("faktum_localdate_erFør_faktum_localdate")
 infix fun Faktum<LocalDate>.erFør(other: Faktum<LocalDate>) =
     PairDomainPredicate(FØR, this, other) { value < other.value }
 
+@JvmName("faktum_localdate_erFør_localdate")
 infix fun Faktum<LocalDate>.erFør(other: LocalDate) =
     PairDomainPredicate(FØR, this, Faktum(other)) { value < other }
 
+@JvmName("faktum_localdate_erEtterEllerLik_faktum_localdate")
 infix fun Faktum<LocalDate>.erEtterEllerLik(other: Faktum<LocalDate>) =
     PairDomainPredicate(ETTER_ELLER_LIK, this, other) { value >= other.value }
 
+@JvmName("faktum_localdate_erEtter_faktum_localdate")
 infix fun Faktum<LocalDate>.erEtter(other: Faktum<LocalDate>) =
+    PairDomainPredicate(ETTER, this, other) { value > other.value }
+
+// yearMonth -> localDate
+@JvmName("faktum_yearmonth_erFørEllerLik_faktum_localdate")
+infix fun Faktum<YearMonth>.erFørEllerLik(other: Faktum<LocalDate>) =
+    PairDomainPredicate(FØR_ELLER_LIK, this, other) { this.value.atDay(1) <= other.value }
+
+@JvmName("faktum_yearmonth_erFør_faktum_localdate")
+infix fun Faktum<YearMonth>.erFør(other: Faktum<LocalDate>) =
+    PairDomainPredicate(FØR, this, other) { value.atDay(1) < other.value }
+
+@JvmName("faktum_yearmonth_erFør_localdate")
+infix fun Faktum<YearMonth>.erFør(other: LocalDate) =
+    PairDomainPredicate(FØR, this, Faktum(other)) { value.atDay(1) < other }
+
+@JvmName("faktum_yearmonth_erEtterEllerLik_faktum_localdate")
+infix fun Faktum<YearMonth>.erEtterEllerLik(other: Faktum<LocalDate>) =
+    PairDomainPredicate(ETTER_ELLER_LIK, this, other) { value.atDay(1) >= other.value }
+
+@JvmName("faktum_yearmonth_erEtter_faktum_localdate")
+infix fun Faktum<YearMonth>.erEtter(other: Faktum<LocalDate>) =
+    PairDomainPredicate(ETTER, this, other) { value.atDay(1) > other.value }
+
+// localDate -> yearMonth
+
+@JvmName("faktum_localdate_erFørEllerLik_faktum_yearmonth")
+infix fun Faktum<LocalDate>.erFørEllerLik(other: Faktum<YearMonth>) =
+    PairDomainPredicate(FØR_ELLER_LIK, this, other) { this.value <= other.value.atDay(1) }
+
+@JvmName("faktum_localdate_erFør_faktum_yearmonth")
+infix fun Faktum<LocalDate>.erFør(other: Faktum<YearMonth>) =
+    PairDomainPredicate(FØR, this, other) { value < other.value.atDay(1) }
+
+@JvmName("faktum_localdate_erFør_yearmonth")
+infix fun Faktum<LocalDate>.erFør(other: YearMonth) =
+    PairDomainPredicate(FØR, this, Faktum(other)) { value < other.atDay(1) }
+
+@JvmName("faktum_localdate_erEtterEllerLik_faktum_yearmonth")
+infix fun Faktum<LocalDate>.erEtterEllerLik(other: Faktum<YearMonth>) =
+    PairDomainPredicate(ETTER_ELLER_LIK, this, other) { value >= other.value.atDay(1) }
+
+@JvmName("faktum_localdate_erEtter_faktum_yearmonth")
+infix fun Faktum<LocalDate>.erEtter(other: Faktum<YearMonth>) =
+    PairDomainPredicate(ETTER, this, other) { value > other.value.atDay(1) }
+
+// YearMonth -> YearMonth
+@JvmName("faktum_yearmonth_erFørEllerLik_faktum_yearmonth")
+infix fun Faktum<YearMonth>.erFørEllerLik(other: Faktum<YearMonth>) =
+    PairDomainPredicate(FØR_ELLER_LIK, this, other) { this.value <= other.value }
+@JvmName("faktum_yearmonth_erFør_faktum_yearmonth")
+infix fun Faktum<YearMonth>.erFør(other: Faktum<YearMonth>) =
+    PairDomainPredicate(FØR, this, other) { value < other.value }
+@JvmName("faktum_yearmonth_erFør_yearmonth")
+infix fun Faktum<YearMonth>.erFør(other: YearMonth) =
+    PairDomainPredicate(FØR, this, Faktum(other)) { value < other }
+@JvmName("faktum_yearmonth_erEtterEllerLik_faktum_yearmonth")
+infix fun Faktum<YearMonth>.erEtterEllerLik(other: Faktum<YearMonth>) =
+    PairDomainPredicate(ETTER_ELLER_LIK, this, other) { value >= other.value }
+@JvmName("faktum_yearmonth_erEtter_faktum_yearmonth")
+infix fun Faktum<YearMonth>.erEtter(other: Faktum<YearMonth>) =
     PairDomainPredicate(ETTER, this, other) { value > other.value }
 
 /**
